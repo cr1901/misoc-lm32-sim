a5dd97568e5da29458807f87309d0bac33fd922d
William D. Jones <thor0505@comcast.net>
2018-06-09 13:39:58 -0400

    Annotate tinyfpga-soc-trigger/config.mk.

--- targets/tinyfpga-soc-trigger/config.mk
+++ targets/tinyfpga-soc-trigger/config.mk
@@ -2,6 +2,7 @@ MY_POST_SYNTH_SOURCES += /c/msys64/mingw32/share/yosys/ice40/cells_sim.v
 
 MY_DATA += targets/$(TARGET)/gateware/mem.init
 
+# Dummy- not used.
 MY_FIRMWARE = targets/$(TARGET)/firmware.elf
 
 TB_TOP=tinyfpga-soc-trigger
